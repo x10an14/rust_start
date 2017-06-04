6a60c77b289a840e80854500addede64f6854afa
x10an14 <x10an14@hotmail.com>
2017-06-04 19:48:35 +0200

    Feature: Let user specify range to guess at start.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
 extern crate rand;
 
 use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+
+
+fn receive_number_from_stdin(print_string: &str) -> u32 {
+    use std::io::Write;
+    use std::process;
+
+    print!("{}: ", print_string);
+    io::stdout().flush().ok().expect("Could not flush stdout");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    let number: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_)  => {
+            let guess = input.trim().to_string();
+            println!("Received '{}' as your input string.", guess);
+            if guess.to_lowercase().starts_with("q") {
+                println!("Quitting game");
+                process::exit(0);
+            } else {
+                println!("Unable to parse!\nTry again:");
+                return receive_number_from_stdin(print_string);
+            }
+        },
+    };
+
+    number
+}
+
 
 fn guess_random_number(to: u32, from: u32) -> bool {
-    let secret_number = rand::thread_rng().gen_range(to, from);
+    use rand::Rng;
+    use std::cmp::Ordering;
 
+    let secret_number = rand::thread_rng().gen_range(to, from);
     loop {
         println!("Please input your guess:");
         let mut guess = String::new();
@@ -41,13 +71,17 @@ fn guess_random_number(to: u32, from: u32) -> bool {
     true
 }
 
+
 fn main() {
     println!("Guess the number(s)!");
-    println!("--------------------");
+    println!("--------------------\n");
+
+    let min_number = receive_number_from_stdin("Specify minimum number");
+    let max_number = receive_number_from_stdin("Specify maximum number");
 
     loop {
         println!("\nGuess the number(!):");
-        if guess_random_number(1, 21) == false {
+        if guess_random_number(min_number, max_number + 1) == false {
             println!("Quitting game");
             break;
         }
